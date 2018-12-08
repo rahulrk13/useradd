dc6194779ccbd4a7b87bd9a09f0a3f5f6fc08234
root <root@rahulk.awsserver.com>
2018-12-08 06:15:01 +0000

    Can validate users based on json file

--- validate.sh
+++ validate.sh
@@ -1,22 +1,33 @@
-u=$1
-n=$(grep $u /etc/passwd | wc -l)
+user_count=$(jq '. | length' newuser.json)
+echo $user_count
+loop_count=0
 
-#if [ grep "testuser1" /etc/passwd | wc -l > 0 ]
-echo $u
-if [ $n = 0 ]
-then
-   echo "New User detected"
-   echo "Do you want to add this user (y/n)"
-   read a
-   if [ $a == "y" ]
-   then
-        echo "You selected Yes, Adding new user now"
-        useradd -s /bin/bash -m $1
-        passwd $1
-        echo "New User \"$1\" was added Successfully!"
-   else
-        echo "You selected no"     
-   fi
-else
-   echo "User exist"
-fi
+while [ $loop_count -lt $user_count ]
+do
+        user_name=$(jq '.'[$loop_count] newuser.json | tr -d "\"")
+        echo $user_name
+        #echo "Current user name $user_name"
+        n=$(grep $user_name /etc/passwd | wc -l)
+        if [ $n -gt 0 ]
+        then
+                echo "User exist"
+        else
+                echo "New User detected: $user_name"
+                echo "Do you want to add this user (y/n)"
+                read a
+                if [ $a == "y" ]
+                then
+                        echo "You selected Yes, Adding new user now"
+                        useradd -s /bin/bash -m $user_name
+                        passwd $user_name
+                        echo "New User \"$user_name\" was added Successfully!"
+                else
+                        echo "You selected no"
+                fi
+        
+        fi
+        loop_count=`expr $loop_count + 1`
+done
+
+echo "
+User Validation complete!"
