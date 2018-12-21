5bdc437304bdadfb34dfc0a44f75e7ad6e1b5548
root <root@rahulk.awsserver.com>
2018-12-21 11:05:13 +0000

    Made script independent of input from user

--- validate.sh
+++ validate.sh
@@ -10,24 +10,27 @@ do
         n=$(grep $user_name /etc/passwd | wc -l)
         if [ $n -gt 0 ]
         then
-                echo "User exist"
+                echo "User exist :$user_name"
         else
                 echo "New User detected: $user_name"
-                echo "Do you want to add this user (y/n)"
-                read a
-                if [ $a == "y" ]
-                then
-                        echo "You selected Yes, Adding new user now"
+               # echo "Do you want to add this user (y/n)"
+               # read a
+               # if [ $a == "y" ]
+               echo "Adding user $user_name"
+	       # then
+                     #   echo "You selected Yes, Adding new user now"
                         useradd -s /bin/bash -m $user_name
                         #passwd $user_name
 			echo "New User \"$user_name\" was added Successfully! Deploying public keys for ssh access..."
-                        mkdir /home/$user_name/.ssh
+                        [ -f ./public_keys/$user_name.pub ] && echo "Public keys exists" || echo "Public keys were not found"
+		       	mkdir /home/$user_name/.ssh
                         cp ./public_keys/$user_name.pub /home/$user_name/.ssh/authorized_keys
                         chown -R $user_name:$user_name /home/$user_name
                         echo "Public keys uploaded successfully!"
-                else
-                        echo "You selected no"
-                fi
+                        echo "___________________________"
+			# else
+                #        echo "You selected no"
+               # fi
         
         fi
         loop_count=`expr $loop_count + 1`
